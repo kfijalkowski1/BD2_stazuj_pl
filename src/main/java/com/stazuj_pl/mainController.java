91bdfa39341c1cd79643c6e00018d7f5f801a840
Kacper <kacper.mur123@gmail.com>
2023-05-02 10:19:48 +0200

    fixed bug with returning html page

--- src/main/java/com/stazuj_pl/mainController.java
+++ src/main/java/com/stazuj_pl/mainController.java
@@ -1,11 +1,10 @@
 package com.stazuj_pl;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class mainController {
-
     @GetMapping(value="/")
     public String hello(){
         return "index";
