967a2aef1ac4a221c921ec915d0a7fd581f8dc9a
rszczep2 <01169247@pw.edu.pl>
2023-06-15 21:35:14 +0200

    implemented and tested Employee

--- src/main/java/com/stazuj_pl/Employee/EmployeeHandler.java
+++ src/main/java/com/stazuj_pl/Employee/EmployeeHandler.java
@@ -1,9 +1,8 @@
-package com.stazuj_pl.Student;
+package com.stazuj_pl.Employee;
 
 
 import com.stazuj_pl.CrudHandler;
 import com.stazuj_pl.EntityObj;
-import com.stazuj_pl.User.User;
 import com.stazuj_pl.User.UserHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -18,19 +17,19 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class StudentHandler extends CrudHandler {
+public class EmployeeHandler extends CrudHandler {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
     @Autowired
     UserHandler userHandler;
 
-    StudentHandler() {
-        this.tableName = "Students";
-        this.tableMainKey = "student_id";
-        this.rowMapper = new BeanPropertyRowMapper<>(Student.class);
+    EmployeeHandler() {
+        this.tableName = "Employees";
+        this.tableMainKey = "employee_id";
+        this.rowMapper = new BeanPropertyRowMapper<>(Employee.class);
         this.modifiableKeys = Arrays.asList("name", "surname", "mail", "login", "photo_path", "about_me",
-                "academic_year", "looking_for_job", "keywords", "academic_info_id");
+                "message_template", "search_number", "plan_type", "company_id");
     }
 
     @Override
@@ -40,14 +39,20 @@ public class StudentHandler extends CrudHandler {
 
     public ResponseEntity<HttpStatus> addEntity(Map<String, Object> data) {
         try {
-            List<String> obligatoryFields = Arrays.asList("hash_password", "name", "surname", "login", "academic_info_id");
-            List<String> optionaryFields = Arrays.asList("mail", "photo_path", "about_me", "academic_year", "looking_for_job", "keywords");
+            List<String> obligatoryFields = Arrays.asList("hash_password", "name", "surname", "login", "company_id", "search_number", "plan_type");
+            List<String> optionaryFields = Arrays.asList("mail", "photo_path", "about_me", "message_template");
+            List<String> plan_typeEnum = Arrays.asList("regular", "premium", "ultimate");
 
             for (String key : obligatoryFields) {
                 if (!data.containsKey(key)) {
                     return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
                 }
             }
+
+            if (!plan_typeEnum.contains(data.get("plan_type"))) {
+                return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+            }
+
             for (String key : optionaryFields) {
                 if (!data.containsKey(key)) {
                     data.put(key, null);
@@ -55,7 +60,7 @@ public class StudentHandler extends CrudHandler {
             }
 
             String sql_user = "INSERT INTO Users (mail, hash_password, name, surname, login, photo_path, about_me) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            String sql_student = "INSERT INTO Students (academic_info_id, user_student_id, academic_year, looking_for_job, keywords) VALUES (?, ?, ?, ?, ?)";
+            String sql_employee = "INSERT INTO Employees (user_employee_id, company_id, message_template, search_number, plan_type) VALUES (?, ?, ?, ?, ?)";
 
             int addUser = jdbcTemplate.update(
                     sql_user,
@@ -72,16 +77,17 @@ public class StudentHandler extends CrudHandler {
                 return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
             }
             int user_id = userHandler.getIdByUniqueField(data.get("login").toString());
-            int addStudent = jdbcTemplate.update(
-                    sql_student,
-                    data.get("academic_info_id").toString(),
+
+            int addEmployee = jdbcTemplate.update(
+                    sql_employee,
                     user_id,
-                    data.get("academic_year").toString(),
-                    data.get("looking_for_job").toString(),
-                    data.get("keywords").toString()
+                    data.get("company_id").toString(),
+                    data.get("message_template").toString(),
+                    data.get("search_number").toString(),
+                    data.get("plan_type").toString()
             );
 
-            if (addStudent == 1) {
+            if (addEmployee == 1) {
                 return new ResponseEntity<HttpStatus>(HttpStatus.OK);
             } else {
                 userHandler.deleteById(user_id);
@@ -91,6 +97,4 @@ public class StudentHandler extends CrudHandler {
             return new ResponseEntity<HttpStatus>(HttpStatus.I_AM_A_TEAPOT);
         }
     }
-
-
 }
